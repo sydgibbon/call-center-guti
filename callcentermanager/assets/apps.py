73a03e41558a714d3fb48eced126fed4e832fff8
Sydney Gibbon <sydney@leadboxhq.com>
2022-08-29 22:42:31 -0300

    renamed "activos" as "assets"

--- callcentermanager/assets/apps.py
+++ callcentermanager/assets/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ActivosConfig(AppConfig):
+class AssetsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'activos'
+    name = 'assets'
