9194dbc027476a21dcdde565bfdd3fdbbbf37152
sydgibbon <57300925+sydgibbon@users.noreply.github.com>
2023-04-30 18:08:44 -0300

    selects for assets, missing phones

--- callcentermanager/assets/passivedcequipments/serializers.py
+++ callcentermanager/assets/passivedcequipments/serializers.py
@@ -1,6 +1,11 @@
 
 from rest_framework import serializers  # import de serializers
-from assets.models import Passivedcequipmenttypes, Passivedcequipmentmodels
+from assets.models import Passivedcequipments, Passivedcequipmenttypes, Passivedcequipmentmodels
+
+class GetPassivedcequipmentsSelectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Passivedcequipments
+        fields = ['id', 'name']
 
 class GetPassivedcequipmenttypesSelectSerializer(serializers.ModelSerializer):
     class Meta:
--- callcentermanager/assets/peripherals/views.py
+++ callcentermanager/assets/peripherals/views.py
@@ -1,8 +1,17 @@
-from assets.peripherals.serializers import GetPeripheraltypesSelectSerializer, GetPeripheralmodelsSelectSerializer
-from assets.models import Peripheraltypes, Peripheralmodels
+from assets.peripherals.serializers import GetPeripheralsSelectSerializer, GetPeripheraltypesSelectSerializer, GetPeripheralmodelsSelectSerializer
+from assets.models import Peripherals, Peripheraltypes, Peripheralmodels
 from rest_framework import viewsets  # import de ViewSets
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
+
+class GetPeripheralsSelectViewSet(viewsets.ViewSet):
+    permission_classes = (IsAuthenticated, AllowAny)
+    http_method_names = ['get']
+
+    def list(self, request, format=None):
+        peripherals = GetPeripheralsSelectSerializer(Peripherals.objects.all(), many=True)
+
+        return Response(peripherals.data)
 
 class GetPeripheraltypesSelectViewSet(viewsets.ViewSet):
     permission_classes = (IsAuthenticated, AllowAny)
