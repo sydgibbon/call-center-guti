936eb72535211c900d0d128be82608988876e293
GusChoque <110579994+GusChoque@users.noreply.github.com>
2023-04-30 17:12:34 -0300

    deleted created files
    all drop down endpopints are created, the only dropdown is hardcoded in the frontend

--- callcentermanager/assets/cartridges/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers  # import de serializers
-from assets.models import Cartridges
-
-class GetCartridgesSelectSerializer(serializers.ModelSerializer):
-    model = Cartridges
-    fields = ('id', 'name')
