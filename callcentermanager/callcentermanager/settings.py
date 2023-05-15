e45f5ee408586276daa74ac2995f7dd755638e9e
Sydney Gibbon <57300925+sydgibbon@users.noreply.github.com>
2023-05-14 22:03:31 -0300

    Cors middleware at the top

--- callcentermanager/callcentermanager/settings.py
+++ callcentermanager/callcentermanager/settings.py
@@ -57,6 +57,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -64,7 +65,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
