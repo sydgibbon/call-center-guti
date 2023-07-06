2bb0b39f9fd71eed45a8a500556c33084760f5d4
sydgibbon <57300925+sydgibbon@users.noreply.github.com>
2023-07-05 22:52:26 -0300

    Installed Django REST Frakework simplejwt extension and implemented api token endpoints

--- callcentermanager/callcentermanager/urls.py
+++ callcentermanager/callcentermanager/urls.py
@@ -19,7 +19,10 @@ from django.urls import path, include
 from .views import Login
 from django.views.generic import TemplateView
 from rest_framework.schemas import get_schema_view
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -36,4 +39,6 @@ urlpatterns = [
         template_name='swagger-ui.html',
         extra_context={'schema_url':'openapi-schema'}
     ), name='swagger-ui'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
