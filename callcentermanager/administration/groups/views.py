0ea32e471a3459cd130e09fd8ef6a0965d3b7869
FranchJavier <franciscojavierlieg@gmail.com>
2023-11-12 21:27:50 -0300

    getGroupsSelect method administration created

---
+++ callcentermanager/administration/groups/views.py
@@ -0,0 +1,14 @@
+from administration.groups.serializers import GetGroupsSelectSerializer
+from assets.models import Groups
+from rest_framework import viewsets  # import de ViewSets
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+
+class GetGroupsSelectViewSet(viewsets.ViewSet):
+    queryset = Groups.objects.all()
+    permission_classes = (IsAuthenticated, AllowAny)
+    http_method_names = ['get']
+
+    def list(self, request, format=None):
+        groups = GetGroupsSelectSerializer(Groups.objects.all(), many=True) 
+        return Response(groups.data)
--- callcentermanager/administration/routers.py
+++ callcentermanager/administration/routers.py
@@ -1,5 +1,8 @@
 from rest_framework.routers import DefaultRouter
 from administration.users import views as users
+from administration.groups import views as groups
+
 
 router=DefaultRouter()
 router.register(r'getUsers', users.GetUsersViewSet, basename='getUsers')
+router.register(r'getGroupsSelect', groups.GetGroupsSelectViewSet, basename='getGroupsSelect')
